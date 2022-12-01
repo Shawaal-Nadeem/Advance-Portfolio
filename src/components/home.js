6dc11d6c9ebf1e78d9abc176b671d5a81b1fc805
Shawaal-Nadeem <hafizshawalnadeem@gmail.com>
2022-12-02 00:54:31 +0500

    Modal apply on Pics

--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,32 @@
 import React from 'react';
-
+import Fade from 'react-reveal/Reveal';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import {useEffect} from "react";
 function Home() {
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : 'transparent',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    boxShadow:'none',
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
+  useEffect(() => {
+  document.querySelectorAll('.image-container img').forEach(image=>{
+image.onclick=()=>{
+  document.querySelector('.popup-image').style.display='block';
+  document.querySelector('.popup-image img').src=image.getAttribute('src');
+}
+  });
+  document.querySelector('.popup-image span').onclick=()=>{
+    document.querySelector('.popup-image').style.display='none';
+ }
+  });
   return (
    <div>
-    <div className='Homepage'>
+    <div className='Video-Section'>
     <video poster="" loop autoPlay muted>
   <source src="/videos/v.webm" type="video/webm"/>
   <source src="/videos/v.mp4" type="video/mp4"/>
@@ -13,7 +36,7 @@ function Home() {
   <nav>
   <ul>
             <li ><a href='#' className='navbtn-home'>Home</a></li>
-            <li><a href='#' className='navbtn'>Portfolio</a></li>
+            <li><a href='#portfolio' className='navbtn'>Portfolio</a></li>
             <li><a href='#' className='navbtn'>Contact</a></li>
             <li><a href='#' className='navbtn'>Socials</a></li>
             <li><a href='#' className='navbtn'>About</a></li>
@@ -29,6 +52,84 @@ function Home() {
     <button className='hirebtn'>Hire Me</button>
     </div>
     </div>
+
+    <Fade top distance='10%' duration ={1500}>
+<div className='portfolio' id='portfolio'>
+<h1 className='portfolio-heading'>PORTFOLIO</h1>
+<div className='portfolioButtons'>
+<button className='GraphicDesign'>Graphic Design</button>
+<p className='lineDesign'><hr className='sline'/></p>
+<button className='GraphicDesign'>Video Editing</button>
+</div>
+</div>
+</Fade>
+
+<div className='showcase'>
+      <Grid container spacing={2}>
+        <Grid item xs={4}>
+          <Item>
+            <h1 className='designHeading'>Designs <br/>&<br/>Artworks</h1>
+          </Item>
+        </Grid>
+        <Grid item xs={8}>
+          <Item>
+            <div className='image-container'>
+    <div className='image'><img src='/images/d1.png'/></div>
+    </div>
+          </Item>
+        </Grid>
+        <Grid  item xs={4}>
+          <Item className='creativityGrid'>
+            <h1 className='creativity'>Creativity</h1>
+          </Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item>
+            <div className='image-container'>
+    <div className='image'><img src='/images/d2.png'/></div>    
+    </div>
+          </Item>
+          <Item>
+            <div className='image-container'>
+    <div className='image'><img src='/images/d4.png'/></div>
+    </div>
+          </Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item>
+            <div className='image-container'>
+    <div className='image'><img src='/images/d3.png'/></div>
+    </div>
+          </Item>
+          <Item>
+            <div className='image-container'>
+    <div className='image'><img src='/images/d5.png'/></div>
+    </div>
+          </Item>
+        </Grid>
+      </Grid>
+      <div className='popup-image'>
+        <span>&times;</span>
+        <img src="/images/d1.png" alt=''/>
+      </div>
+      <div className='popup-image'>
+        <span>&times;</span>
+        <img src="/images/d2.png" alt=''/>
+      </div>
+      <div className='popup-image'>
+        <span>&times;</span>
+        <img src="/images/d3.png" alt=''/>
+      </div>
+      <div className='popup-image'>
+        <span>&times;</span>
+        <img src="/images/d4.png" alt=''/>
+      </div>
+      <div className='popup-image'>
+        <span>&times;</span>
+        <img src="/images/d5.png" alt=''/>
+      </div>
+</div>
+
    </div>
   );
 }
